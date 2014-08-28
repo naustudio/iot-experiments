e0323c0c951c1c2950806f8211f07718f15d8f85
Thinh Le <thinh.le@naustud.io>
2014-08-28 10:41:14 +0700

    Create motion detector updates on web

---
+++ motion-detector-web/app.js
@@ -0,0 +1,50 @@
+var http = require('http');
+var sys = require('sys');
+var fs = require('fs');
+
+http.createServer(function(req, res) {
+  debugHeaders(req);
+
+  if (req.headers.accept && req.headers.accept === 'text/event-stream') {
+    if (req.url === 'https://api.spark.io/v1/events/motion-detected?access_token=5a4501e8e5d6ab780731274e000a5894657f9d10') {
+      sendSSE(req, res);
+    } else {
+      res.writeHead(404);
+      res.end();
+    }
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(fs.readFileSync(__dirname + '/index.html'));
+    res.end();
+  }
+}).listen(8000);
+
+function sendSSE(req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive'
+  });
+
+  var id = (new Date()).toLocaleTimeString();
+
+  // Sends a SSE every 5 seconds on a single connection.
+  setInterval(function() {
+    constructSSE(res, id, (new Date()).toLocaleTimeString());
+  }, 5000);
+
+  constructSSE(res, id, (new Date()).toLocaleTimeString());
+}
+
+function constructSSE(res, id, data) {
+  res.write('id: ' + id + '\n');
+  res.write("data: " + data + '\n\n');
+}
+
+function debugHeaders(req) {
+  sys.puts('URL: ' + req.url);
+  for (var key in req.headers) {
+    sys.puts(key + ': ' + req.headers[key]);
+  }
+  sys.puts('\n\n');
+}
